bed121b528755a6fca59e53206c13d21626c6dc2
pabsoftware <pabsoftware@outlook.com>
2024-02-25 13:43:02 -0300

    primeiro comit exercicio vueJS

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { reactive } from 'vue';
+import Formulario from './components/Formulario.vue';
+
+const estado =reactive ({
+  valor_x : null,
+  valor_y : null,
+  resultado: 0 ,
+  tipoOperacao : '',
+ 
+})
+
+const executarOperacao = (operacao, x, y) => {
+    x = parseFloat(estado.valor_x);
+    y = parseFloat(estado.valor_y);
+    operacao = estado.tipoOperacao;
+ 
+    switch (operacao){
+      case '+': return ( estado.resultado = x + y);
+      case '-': return ( estado.resultado = x - y);
+      case '*': return ( estado.resultado = x * y);
+      case '/': return ( estado.resultado = x / y);
+    }
+}
+
+
+
+</script>
+
+<template>
+
+ <div class = "container card mt-5 ">
+  <Formulario :selecionar-operacao = "evento => estado.tipoOperacao = evento.target.value"
+              :valor-x ="evento => estado.valor_x = evento.target.value"
+              :valor-y ="evento => estado.valor_y = evento.target.value" 
+              :executar-operacao="executarOperacao()"
+              :resultado = "estado.resultado"
+              
+              />
+       
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps([ 'selecionarOperacao', 'valorX', 'valorY', 'executarOperacao', 'resultado' ])
+</script>
+
+<template>
+    <div class = "container card ">
+        <form >
+            
+                <div class = "row card mb-4">
+                    <div class = "col badge bg-secondary">
+                        <h2>Calculadora</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 mb-2">
+                        <input @change = "props.valorX"   required class="form-control" type="number" placeholder="Valor x">
+                    </div>
+                    <div class="col-md-4">
+                        <input @change = "props.valorY"   required class="form-control mb-2" type="number" placeholder="Valor y">
+                    </div>
+
+                    <div class="col-md-4 mb-2">
+                        <select @click = "props.executarOperacao"  @change = "props.selecionarOperacao" class="form-control" name="" id="">
+                            <option value="">Selecione a operação</option>
+                            <option value="+">Somar</option>
+                            <option value="-">Subtrair</option>
+                            <option value="*">Multiplicar</option>
+                            <option value="/">Dividir</option>
+                        </select>
+                    </div>
+                    
+                </div>
+                <div class = "row">
+                    <div class="col-mb-3  ">
+                        <h4 class = "card p-2" v-if = " resultado || resultado === 0" >Resultado: {{ props.resultado }} </h4>
+                        <h4 class = "card p-2" v-else >O valor de x e y não podem está vazios </h4>
+                    </div>
+                </div>
+        
+        </form>
+    </div>
+</template>
+
